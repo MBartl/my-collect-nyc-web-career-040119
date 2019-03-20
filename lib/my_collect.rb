1bf053fa0ca47362ab27c7bdba580baec6c370b4
Matthew Bartl <mattbartl@gmail.com>
2019-03-20 17:04:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length do
-    for element in array
-      new_array.push yield element
+    new_array.push yield element
     end
     i += 1
   end
